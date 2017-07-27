f48211a6d5eadd34be554a5eb4f5f0fb68ee192f
Anna Bezrukova <annelia_55@mail.ru>
2017-07-27 17:32:35 +0700

    counters javascript added

---
+++ js/index.js
@@ -0,0 +1,51 @@
+window.onload=function() {
+    // Month,Day,Year,Hour,Minute,Second
+    upTime('august,04,2017,00:00:00');
+};
+
+function upTime(countTo) {
+    now = new Date();
+    countTo = new Date(countTo);
+    difference = (now - countTo);
+    days = Math.floor(difference/(60*60*1000*24)) + 1;
+
+    hours=Math.floor((difference%(60*60*1000*24))/(60*60*1000)) + 1;
+    mins=Math.floor(((difference%(60*60*1000*24))%(60*60*1000))/(60*1000)) + 1;
+    secs=Math.floor((((difference%(60*60*1000*24))%(60*60*1000))%(60*1000))/1000) + 1;
+
+    // If the count down is finished, write some text
+    if (difference < 0) {
+        document.getElementById('days').firstChild.nodeValue = -days;
+        document.getElementById('hours').firstChild.nodeValue = -hours;
+        document.getElementById('minutes').firstChild.nodeValue = -mins;
+        document.getElementById('seconds').firstChild.nodeValue = -secs;
+
+        document.getElementById('days-2').firstChild.nodeValue = -days;
+        document.getElementById('hours-2').firstChild.nodeValue = -hours;
+        document.getElementById('minutes-2').firstChild.nodeValue = -mins;
+        document.getElementById('seconds-2').firstChild.nodeValue = -secs;
+
+        document.getElementById('days-3').firstChild.nodeValue = -days;
+        document.getElementById('hours-3').firstChild.nodeValue = -hours;
+        document.getElementById('minutes-3').firstChild.nodeValue = -mins;
+        document.getElementById('seconds-3').firstChild.nodeValue = -secs;
+    } else {
+        document.getElementById('days').firstChild.nodeValue = 0;
+        document.getElementById('hours').firstChild.nodeValue = 0;
+        document.getElementById('minutes').firstChild.nodeValue = 0;
+        document.getElementById('seconds').firstChild.nodeValue = 0;
+
+        document.getElementById('days-2').firstChild.nodeValue = 0;
+        document.getElementById('hours-2').firstChild.nodeValue = 0;
+        document.getElementById('minutes-2').firstChild.nodeValue = 0;
+        document.getElementById('seconds-2').firstChild.nodeValue = 0;
+
+        document.getElementById('days-3').firstChild.nodeValue = 0;
+        document.getElementById('hours-3').firstChild.nodeValue = 0;
+        document.getElementById('minutes-3').firstChild.nodeValue = 0;
+        document.getElementById('seconds-3').firstChild.nodeValue = 0;
+    }
+
+    clearTimeout(upTime.to);
+    upTime.to=setTimeout(function(){ upTime(countTo); },1000);
+}
